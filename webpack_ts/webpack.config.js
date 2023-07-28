386874d03bde043104f44bd13e6e506f1fcff524
Regan Y <hens.virtuella.varld@gmail.com>
2023-07-28 14:37:52 +0200

    fix: :bug: add the missing code for the original code
    Colt didn't mention in the couse video that he added devServer config in webpack.config.js

--- webpack_ts/webpack.config.js
+++ webpack_ts/webpack.config.js
@@ -1,10 +1,14 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
   devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './'),
+    },
+  },
   module: {
     rules: [
       {
@@ -20,6 +24,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
   },
-  plugins: [new HtmlWebpackPlugin()],
 };
